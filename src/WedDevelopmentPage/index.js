1ca21787007b242c6c60633524e5041ab4f20a97
Vũ Tiến Đức <107258145+TanPhatStore@users.noreply.github.com>
2023-07-17 22:43:47 +0700

    upload

---
+++ src/WedDevelopmentPage/index.js
@@ -0,0 +1,17 @@
+
+import './weddevelopment.scss'
+
+function WedDevelopment({user}) {
+    return (
+        <div id='wed-development'>
+            <div className='title'>
+                Wed UI Design
+            </div>
+            <div className='description'>
+                
+            </div>
+        </div>
+    );
+}
+
+export default WedDevelopment;
